45f371fa8c63bfa6c96a1416cc3f2669879f13d4
KRITANGAUTAM7 <97915628+KRITANGAUTAM7@users.noreply.github.com>
2022-02-01 03:22:01 +0545

    Create Kritan.py

---
+++ Kritan.py
@@ -0,0 +1,100 @@
+!/usr/bin/python2
+
+ coding=utf-8
+
+ Coded By Kritan XD
+
+ My Facebook ( https://www.facebook.com/MR.KRITAN77 )
+
+ 
+
+      (C) Copyright 407 Authentic Exploit
+
+      Rebuild Copyright Can't make u real programmer:)
+
+      Coded By Kritan XD 
+      import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+
+from multiprocessing.pool import ThreadPool
+
+from requests.exceptions import ConnectionError
+
+from mechanize import Browser 
+ 
+ reload(sys)
+
+sys.setdefaultencoding('utf8')
+
+br = mechanize.Browser()
+
+br.set_handle_robots(False)
+
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+
+br.addheaders = [('User-Agent', 'Mozilla/5.0 (Linux; Android 9; CPH1923 Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/85.0.4183.81 Mobile Safari/537.36')] 
+
+def keluar():
+
+	print "\033[0;39m[!] \x1b[0;39mExit"	
+  os.sys.exit() 
+
+def acak(b): 
+w = 'ahtdzjc'
+d = "
+for i in x:
+d += '!'+w[random.randint(0,len(w)-1)]+i 
+return cetak(d)
+def cetak(b):
+w = 'ahtdzjc'
+for i in w:
+ j = w.index(i)
+ x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+ x += '\033[0m'
+ x = x.replace('!0','\033[0m')
+ sys.stdout.write(x+'\n')
+ def jalan(z):
+ for e in z + '\n':
+ sys.stdout.write(e)
+ sys.stdout.flush()
+ time.sleep(00000.1)
+  
+##### LOGO  #####
+logo = """ 
+\33[38;1m KRITAN GAUTAM
+\33[38;1m NEP ̳̳H̳A̳C̳K̳E̳R̳̳
+\33[38;1mW̳E̳ ̳A̳R̳E̳ ̳A̳N̳O̳N̳Y̳M̳O̳U̳S 
+\33[38;1mW̳E̳ ̳A̳R̳E̳ ̳L̳E̳G̳I̳O̳N̳̳ 
+\33[38;1m0̳3̳2̳3̳2̳1̳3̳2̳3̳6̳2̳
+\33[38;1mLoaded....100%
+
+\033[0;35m[\033[0;92m •• \033[0;35m] Author   : KRITAN GAUTAM
+\033[1;34m\033[1;41;33mヅ︻写፨丐יי一一一 ҉ ㄹㅇㄹㅇ \033[0m"""
+def tik():
+titik = ['.   ','..  ','... ']
+for o in titik:
+print("\r\x1b[0;39mPlease Wait \x1b[0;39m"+o),;sys.stdout.flush();time.sleep(1)
+ 
+back = 0
+berhasil = []
+KRITAN-CP = []
+KRITAN-OK = []
+id = []
+listgrup = []
+vulnot = "\033[31mNot Vuln"
+vuln = "\033[32mVuln"
+
+os.system("clear")
+print  """
+
+
+
+\033[1;92m♡︎==========================================================================
+\033[1;92m{~} \033[0;96mCreater > \x1b[1;91m༒︎𝐊𝐑𝐈𝐓𝐀𝐍-𝐗𝐃
+\033[1;92m{~} \033[0;96mFacebook > \x1b[1;91m( https://www.facebook.com/MR.KRITAN77 ) 
+\033[1;92m{~} \033[0;96mInstagram > \x1b[1;91mKritan00007
+\033[1;92m♡︎==========================================================================
+def main():
+os.system("clear")
+print(logo)
+print(" \x1b[1;92m    \tMain menu")
+print("")
